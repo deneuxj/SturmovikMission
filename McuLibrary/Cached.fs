2102c597b7154217a17762c9e8ab41f28d68afb9
Johann Deneux <johann.deneux@gmail.com>
2020-02-26 16:08:49 +0100

    Preparing version 6
    Updated the type provider to use the TypeProvider Sdk from paket.
    
    Breaking changes so far:
    - Moved Cached module one level up in the namespace hierarchy
    - Removed type provider argument to control the generation of expressions
      and functions

--- McuLibrary/Cached.fs
+++ McuLibrary/Cached.fs
@@ -15,7 +15,7 @@
 //    You should have received a copy of the GNU Lesser General Public License
 //    along with SturmovikMission.  If not, see <http://www.gnu.org/licenses/>.
 
-module SturmovikMission.DataProvider.Cached
+module SturmovikMission.Cached
 
 open System.Collections.Generic
 
